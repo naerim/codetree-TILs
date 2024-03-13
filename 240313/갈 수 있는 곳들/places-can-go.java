90097107c703331ba731859314589d6b1e9e8fc1
김내림 <naerim1119@gmail.com>
2024-03-13 21:57:25 +0900

    Title: 갈 수 있는 곳들, Time: 271ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 240313/갈 수 있는 곳들/places-can-go.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class Main {
+
+    public static class Point {
+        int x;
+        int y;
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static int n, k;
+    public static boolean visited[][];
+    public static int map[][];
+    public static Point startArr[];
+    public static int dx[] = new int[]{0,1,0,-1};
+    public static int dy[] = new int[]{1,0,-1,0};
+    public static Queue<Point> queue = new LinkedList();
+
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        k = sc.nextInt();
+    
+        map = new int[n+1][n+1];
+        visited = new boolean[n+1][n+1];
+
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+                map[i][j] = sc.nextInt();
+            }
+        }
+
+        startArr = new Point[k];
+        for(int i=0; i<k; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            Point p = new Point(x, y);
+            queue.add(p);
+            startArr[i] = p;
+        }
+
+        int sum = 0;
+
+        while(!queue.isEmpty()) {
+            Point p = queue.poll();
+            visited[p.x][p.y] = true;
+
+            for(int i=0; i<4; i++) {
+                int nx = p.x + dx[i];
+                int ny = p.y + dy[i];
+                if(nx>0 && nx<=n && ny>0 && ny<=n && map[nx][ny]==0 && !visited[nx][ny]) {
+                    queue.add(new Point(nx, ny));
+                    visited[nx][ny] = true;
+                    sum += 1;
+                }
+            }
+        }
+
+        System.out.print(sum+1);
+    }
+}
